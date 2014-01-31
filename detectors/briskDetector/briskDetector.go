5b4b40149b7d650fcf40cfcd0c46271249ab02ca
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-01-31 05:11:53 -0700

    Add Keypoints method to brisk detector and test.

--- detectors/briskDetector/briskDetector.go
+++ detectors/briskDetector/briskDetector.go
@@ -1,4 +1,4 @@
-// briskDetector package provides the beginning of a brisk detector implementation.
+// briskDetector package provides the very basic beginning of a brisk detector implementation.
 package briskDetector
 
 import (
@@ -7,7 +7,7 @@ import (
 
 const (
   r = 4
-  n = 9
+  n = 12
 )
 
 type Detector struct {
@@ -23,20 +23,39 @@ func RGBPointValue(x, y int, d *Detector) (val int) {
   return int(r/3 + g/3 + b/3)
 }
 
-// examineArea 
-func (d *Detector) examineArea(x int, y int) (above, below bool) {
+// Keypoints launches a go routine which fills the passed in channel of points.
+func (d *Detector)Keypoints(ch chan image.Point){
+  go d.fillKeypointBuffer(ch)
+}
+
+// fillKeypointBuffer loops through all pixels in detector.img and adds to chan
+// if it was a keypoint.
+func (d *Detector)fillKeypointBuffer( ch chan image.Point){
+  for x := d.img.Bounds().Min.X; x < d.img.Bounds().Max.X; x++{
+    for y := d.img.Bounds().Min.Y; y < d.img.Bounds().Max.Y; y++{
+      if d.examineArea(x,y){
+        ch <- image.Point{x,y}
+      }
+    }
+  }
+  close(ch)
+}
+
+// examineArea looks in the area surrounding x,y.
+// if n points are above or n points are below, return keypoint=true.
+func (d *Detector) examineArea(x int, y int) (keypoint bool) {
   // if within 3 of one of the edges, don't do anything (yet)
-  if d.img.Bounds().Max.X+r < x {
-    return false, false
+  if d.img.Bounds().Max.X-r < x {
+    return false
   }
   if d.img.Bounds().Min.Y+r > y {
-    return false, false
+    return false
   }
-  if d.img.Bounds().Min.X-r > x {
-    return false, false
+  if d.img.Bounds().Min.X+r > x {
+    return false
   }
   if d.img.Bounds().Max.Y-r < y {
-    return false, false
+    return false
   }
 
   abv := uint(0)
@@ -54,10 +73,11 @@ func (d *Detector) examineArea(x int, y int) (above, below bool) {
       bel++
     }
   }
+
   if abv > n {
-    return true, false
+    return true
   } else if bel > n {
-    return false, true
+    return true
   }
-  return false, false
+  return false
 }
--- detectors/briskDetector/briskDetector_test.go
+++ detectors/briskDetector/briskDetector_test.go
@@ -2,6 +2,7 @@ package briskDetector
 
 import (
   "image"
+  "strconv"
   "testing"
 )
 
@@ -22,12 +23,9 @@ func TestExamineAreaBelow(t *testing.T) {
   d.threshold = 40
   d.img = img
   d.Value = RGBPointValue
-  abv, bel := d.examineArea(16, 16)
-  if abv {
-    t.Error("Above should have returned false")
-  }
-  if !bel {
-    t.Error("Below should have returned true")
+  keypoint := d.examineArea(16, 16)
+  if !keypoint {
+    t.Fatal("Area should return as keypoint")
   }
 }
 
@@ -48,11 +46,37 @@ func TestExamineAreaAbove(t *testing.T) {
   d.threshold = 40
   d.img = img
   d.Value = RGBPointValue
-  abv, bel := d.examineArea(16, 16)
-  if !abv {
-    t.Error("Above should have returned true")
+  keypoint := d.examineArea(16, 16)
+  if !keypoint {
+    t.Fatal("Area should return as keypoint")
   }
-  if bel {
-    t.Error("Below should have returned false")
+}
+
+func TestKeypoints(t *testing.T) {
+  img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{32, 32}})
+
+  for i := 0; i <= 32; i++ {
+    for j := 0; j <= 32; j++ {
+      if j >= 16 && i >= 16 {
+        img.Set(i, j, image.Black)
+      } else {
+        img.Set(i, j, image.White)
+      }
+    }
+  }
+
+  d := Detector{}
+  d.threshold = 40
+  d.img = img
+  d.Value = RGBPointValue
+
+  ch := make(chan image.Point)
+  d.Keypoints(ch)
+  i := 0
+  for _ = range ch {
+    i++
+  }
+  if i != 1 {
+    t.Fatal("Expected one keypoint from image with single corner. Instead " + strconv.Itoa(i) + " were found. ")
   }
 }
