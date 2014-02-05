ff31d6244b5bcf5da930e41e6ac56110a0a5ecc9
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-04 23:59:40 -0700

    Diamond offset generator working for odd radiuses.

--- diamond.go
+++ diamond.go
@@ -2,34 +2,40 @@ package cv
 
 // CircleOffset calculates the offsets for x and y.
 func DiamondOffset(index int, radius int) (int, int) {
-  v1 := radius - (index / 4)
-  v2 := (index / 4)
+  v1 := radius - ((index + 4) / 8)
+  v2 := (index + 4) / 8
 
-  if (index/2)%4 == 0 {
+  if (index/2)%2 == 1 {
     v1 *= -1
-  }else if (index/2)%4 == 0 {
-    v2 *= -1
-  }else if (index/2)%4 == 3 {
-    v1 *= -1
+  }
+  if (index/4)%2 == 1 {
     v2 *= -1
   }
 
-  if index%2 == 0 {
+  if index%2 == 1 {
     return v2, v1
   } else {
     return v1, v2
   }
 }
 
+// every 2 flip v1 sign
+// every 4 flip v2 sign
+// every 8 dec v2 and inc v1
+
 /*
 3,0
 0,3
-2,1
-1,2
+-3,0
+0,-3
+
 2,-1
 -1,2
 -2,-1
 -1,-2
+
+2,1
+1,2
 -2,1
 1,-2
 
--- diamond_test.go
+++ diamond_test.go
@@ -8,8 +8,8 @@ import (
 func TestDiamondOffset(t *testing.T) {
   //perimeter size should always be
 
-  for i := 0; i < 8; i++ {
-    x, y := DiamondOffset(i, 2)
+  for i := 0; i < 12; i++ {
+    x, y := DiamondOffset(i, 3)
     fmt.Print(x)
     fmt.Print(" ")
     fmt.Println(y)
