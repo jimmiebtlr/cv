9ebedab24e4edf01a7a3cc60691ac503d95291f6
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-05 07:46:34 -0700

    Add test for diamondOffset

--- diamond_test.go
+++ diamond_test.go
@@ -1,17 +1,35 @@
 package cv
 
 import (
-  "fmt"
   "testing"
 )
 
 func TestDiamondOffset(t *testing.T) {
   //perimeter size should always be
+  expected := [][]int{
+    []int{3, 0},
+    []int{0, 3},
+    []int{-3, 0},
+    []int{0, -3},
+    []int{2, -1},
+    []int{-1, 2},
+    []int{-2, -1},
+    []int{-1, -2},
+    []int{2, 1},
+    []int{1, 2},
+    []int{-2, 1},
+    []int{1, -2},
+  }
 
-  for i := 0; i < 12; i++ {
-    x, y := DiamondOffset(i, 3)
-    fmt.Print(x)
-    fmt.Print(" ")
-    fmt.Println(y)
+  index := 0
+  for i, v := range expected {
+    actual_x, actual_y := DiamondOffset(i, 3)
+    if v[0] != actual_x || v[1] != actual_y {
+      t.Fatalf("expected: %i,%i + actual: %i,%i", v[0], v[1], actual_x, actual_y)
+    }
+    index++
+  }
+  if index != 12 {
+    t.Fatal("there should be exactly 12 offsets generated")
   }
 }
