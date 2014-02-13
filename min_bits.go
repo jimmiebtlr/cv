69167e0f29fac7980c1f5d8ab8ebaf1005ffee15
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-13 08:09:16 -0700

    More on minBits

--- min_bits.go
+++ min_bits.go
@@ -1,9 +1,37 @@
 package cv
 
+import (
+  "strconv"
+)
+
 // MinBits returns the smallest possible value while not
 // changing the order of the bits.  It does however allow
 // reversal of the bits.
-func MinBits(value int)(min int, reversed bool ){
+func MinBits(value int) (min int, reversed bool) {
+  bits := strconv.FormatInt(int64(value), 2)
+
+  maxStart := 0
+  maxLen := 0
+
+  curStart := 0
+  curLen := 0
+
+  for i := 0; i < len(bits)*2; i++ {
+    if bits[i] == '0' {
+      if curLen == 0 {
+        curStart = i
+      }
+      curLen++
+    } else {
+      if maxLen < curLen {
+        maxLen = curLen
+        maxStart = curStart
+        curLen = 0
+        curStart = 0
+      }
+    }
+  }
+
   // find longest string of zero's
   // determine if reverse is smaller than normal ordering
   return 0, false
