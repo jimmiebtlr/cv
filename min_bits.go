38751ec0856f9878dd5459ec6c61739d9644ae3b
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-15 16:17:51 -0700

    Breaking into functions.  Add more testing to min_bits.

--- min_bits.go
+++ min_bits.go
@@ -2,14 +2,58 @@ package cv
 
 import (
   "strconv"
+  "fmt"
 )
 
 // MinBits returns the smallest possible value while not
 // changing the order of the bits.  It does however allow
 // reversal of the bits.
-func MinBits(value int) (min int, reversed bool) {
-  bits := strconv.FormatInt(int64(value), 2)
+func MinBits(value uint) (min uint, reversed bool) {
+  bits := fullLenUintStr( value )
 
+  zerosStart, zerosEnd := maxZerosRun( bits )
+
+  minStr := bits[zerosStart:] + bits[:zerosEnd]
+
+
+  mintmp,err := strconv.ParseUint( minStr, 2, 64)
+
+  min = uint( mintmp )
+  if err != nil {
+    // rabble
+  }
+
+  minRevStr := ""
+  for _,v := range minStr {
+    minRevStr = string(v) + minRevStr
+  }
+  fmt.Println( minRevStr )
+  mintmpRev, err := strconv.ParseUint( minRevStr, 2, 64 )
+
+  if mintmpRev < mintmp {
+    min = uint(mintmpRev)
+  }else{
+    min = uint(mintmp)
+  }
+
+  // find longest string of zero's
+  // determine if reverse is smaller than normal ordering
+  return min, false
+}
+
+// fullLenUintStr returns the value as binary string 
+// with precending 0's correctly filled in
+func fullLenUintStr( v uint )( vstr string ){
+  bits := strconv.FormatUint(uint64(v), 2)
+  for i := len(bits); i < 32; i++ {
+    bits = "0" + bits
+  }
+  return bits
+}
+
+// maxZerosRun returns the start and end index of the larges consecutive
+// run of zeros in the string
+func maxZerosRun( bits string )( start, end int ){
   maxStart := 0
   maxLen := 0
 
@@ -28,23 +72,12 @@ func MinBits(value int) (min int, reversed bool) {
       if maxLen < curLen {
         maxLen = curLen
         maxStart = curStart
-        curLen = 0
-        curStart = 0
       }
+      curLen = 0
+      curStart = 0
     }
+    fmt.Printf( "%v %v \n", curStart, curLen )
+    fmt.Printf( "%v \n", bits[i%l] )
   }
-
-  minStr := bits[maxStart:]
-  if maxStart > 0 {
-    minStr += bits[:maxStart - 1]
-  }
-  mintmp,err := strconv.ParseInt( minStr, 2, 64)
-  min = int( mintmp )
-  if err != nil {
-    // rabble
-  }
-
-  // find longest string of zero's
-  // determine if reverse is smaller than normal ordering
-  return min, false
+  return maxStart, (maxStart+maxLen) % len( bits )
 }
