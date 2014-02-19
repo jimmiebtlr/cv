71f0d79c1a11e4db60c97c483557b9ef6d494736
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-18 19:52:02 -0700

    Begin CalcDescriptors function.

--- descriptors/descriptor.go
+++ descriptors/descriptor.go
@@ -1,14 +1,28 @@
 package briskDescriptor
 
-import(
+import (
+  "github.com/jimmiebtlr/cv"
   "image"
 )
 
-type Descriptor struct{
-  int8
+type Descriptor struct {
+  uint16
 }
 
-func CalcDescriptors( points chan image.Point, descr chan Descriptor ){
+func CalcDescriptors(points chan image.Point, descr chan Descriptor) {
+  for p := range points {
+    calcDescriptor(p)
+  }
+
   // Calculate multiple rotations
+  rawDesc := uint(0)
+  for i := 0; i < 16; i++ {
+
+  }
+
   // compact and order them
+}
+
+func calcDescriptor(points image.Point) (desc Descriptor) {
+  return desc
 }
