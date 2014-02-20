184b75ee0d0cde4ba2348459c8fa3a671b01e0d9
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2014-02-19 23:01:16 -0700

    More work on descriptor Calc function.

--- descriptors/descriptor.go
+++ descriptors/descriptor.go
@@ -3,26 +3,40 @@ package briskDescriptor
 import (
   "github.com/jimmiebtlr/cv"
   "image"
+  "math"
+)
+
+const (
+  r = 3
 )
 
 type Descriptor struct {
   uint16
 }
 
-func CalcDescriptors(points chan image.Point, descr chan Descriptor) {
+type PxVal func(x, y int, img image.Image) (val int)
+
+func CalcDescriptors(points chan image.Point, img image.Image, descr chan Descriptor, f PxVal) {
   for p := range points {
-    calcDescriptor(p)
+    descr <- calcDescriptor(p, img, f)
   }
-
-  // Calculate multiple rotations
-  rawDesc := uint(0)
-  for i := 0; i < 16; i++ {
-
-  }
-
-  // compact and order them
 }
 
-func calcDescriptor(points image.Point) (desc Descriptor) {
+func calcDescriptor(point image.Point, img image.Image, f PxVal) (desc Descriptor) {
+  v := uint16(0)
+  for i := 0; i < 16; i++ {
+    v += uint16(math.Pow(float64(pointVal(i, point, img, f)), float64(i)))
+  }
   return desc
+}
+
+// pointVal returns 1 if the index in question is greater than point and
+// 0 if it is less
+func pointVal(i int, point image.Point, img image.Image, f PxVal) (val int) {
+  dx, dy := cv.DiamondOffset(i, r)
+  if f(point.X, point.Y, img) < f(point.X+dx, point.Y+dy, img) {
+    return 0
+  } else {
+    return 1
+  }
 }
